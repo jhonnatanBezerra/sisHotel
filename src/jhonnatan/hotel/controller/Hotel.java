cabc94356d7a0961b41fecd6595aeea580255768
Jhonnatan <Jhonnatan@Jhonnatan-PC>
2019-11-23 22:08:20 -0300

    Tela de login CRIADA

---
+++ src/jhonnatan/hotel/controller/Hotel.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jhonnatan.hotel.controller;
+
+
+import java.sql.SQLException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+/**
+ *
+ * @author Jhonnatan
+ */
+public class Hotel extends Application{
+
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        AnchorPane root = FXMLLoader.load(getClass().getResource("/jhonnatan/hotel/view/LoginFXML.fxml"));
+        Scene cena = new Scene(root);
+        primaryStage.setScene(cena);
+        primaryStage.show();
+    }
+    
+    
+    
+    public static void main(String[] args) throws SQLException {
+        launch(args);
+    }
+
+   
+}
---
+++ src/jhonnatan/hotel/controller/LoginController.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jhonnatan.hotel.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.beans.property.DoubleProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Jhonnatan
+ */
+public class LoginController implements Initializable {
+
+   
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    
+}
