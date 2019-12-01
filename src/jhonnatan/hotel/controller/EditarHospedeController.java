cfd05578dedd015b3b9afc94dd04dca6368fe74f
Jhonnatan <Jhonnatan@192.168.100.3>
2019-12-01 01:39:11 -0300

    Utilisando a funçao para editar um hospede

--- src/jhonnatan/hotel/controller/EditarHospedeController.java
+++ src/jhonnatan/hotel/controller/EditarHospedeController.java
@@ -6,13 +6,19 @@
 package jhonnatan.hotel.controller;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javax.swing.JOptionPane;
+import jhonnatan.hotel.dao.HospedeDao;
+import jhonnatan.hotel.model.Hospede;
 
 /**
  * FXML Controller class
@@ -47,21 +53,82 @@ public class EditarHospedeController implements Initializable {
     private TextField txtCEP;
     @FXML
     private TextField txtEstado;
+    
+    
+    private static Hospede hospEdit;
+
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        System.out.println(hospEdit.getNome());
+        initTableUpdate();
     }    
 
     @FXML
     private void cancelarAtualizacao(ActionEvent event) {
+        HospedeController root = new HospedeController();
+        root.abreHospedeController(event);
     }
 
     @FXML
     private void editarHospede(ActionEvent event) {
+        HospedeDao hDao = new HospedeDao();
+        Hospede h = new Hospede();
+        h.setID(Integer.parseInt(lblID.getText()));
+        h.setNome(txtNome.getText());
+        h.setRG(txtRG.getText());
+        h.setCPF(txtCPF.getText());
+        h.setTelefone(txtTelefone.getText());
+        h.setEmail(txtEmail.getText());
+        h.setDataNascimento(java.sql.Date.valueOf(txtDataNascimento.getValue()));
+        h.setLogradouro(txtLogradouro.getText());
+        h.setNumero(txtNumero.getText());
+        h.setBairro(txtBairro.getText());
+        h.setCidade(txtCidade.getText());
+        h.setCEP(Integer.parseInt(txtCEP.getText()));
+        h.setEstado(txtEstado.getText());
+        h.setStatus("1");
+        
+        try {
+            hDao.atualizar(h);
+            JOptionPane.showMessageDialog(null, "Atualização realizada com sucesso!!!");
+            HospedeController root = new HospedeController();
+            root.abreHospedeController(event);
+        } catch (SQLException ex) {
+            Logger.getLogger(EditarHospedeController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
+    
+    public void initTableUpdate(){
+        
+
+        lblID.setText(hospEdit.getID().toString());
+        txtNome.setText(hospEdit.getNome());
+        txtRG.setText(hospEdit.getRG());
+        txtCPF.setText(hospEdit.getCPF());
+        txtTelefone.setText(hospEdit.getTelefone());
+        txtEmail.setText(hospEdit.getEmail());
+        txtDataNascimento.setValue(hospEdit.getDataNascimento().toLocalDate());
+        txtLogradouro.setText(hospEdit.getLogradouro());
+        txtNumero.setText(hospEdit.getNumero());
+        txtBairro.setText(hospEdit.getBairro());
+        txtCidade.setText(hospEdit.getCidade());
+        txtCEP.setText(Integer.toString(hospEdit.getCEP()));
+        txtEstado.setText(hospEdit.getEstado());
+    }
+    
+    public static Hospede getHospEdit() {
+        return hospEdit;
+    }
+
+    public static void setHospEdit(Hospede hospEdit) {
+        EditarHospedeController.hospEdit = hospEdit;
+    }
+    
+    
     
 }
