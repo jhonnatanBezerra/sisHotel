a0512f1078dbfe6dda8da16c3b0e8b9158f38abb
Admin <Admin@DESKTOP-QP8I66M>
2019-12-02 14:27:50 -0300

    Atualização nos metodos salvar e listar; Implementado metodo buscar

--- src/jhonnatan/hotel/dao/HospedagemDao.java
+++ src/jhonnatan/hotel/dao/HospedagemDao.java
@@ -6,21 +6,14 @@
 package jhonnatan.hotel.dao;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javafx.util.converter.LocalDateStringConverter;
 import jhonnatan.hotel.jdbc.ConnectionFactory;
 import jhonnatan.hotel.model.Hospedagem;
-import jhonnatan.hotel.model.Hospede;
 import jhonnatan.hotel.model.Usuario;
 import jhonnatan.hotel.util.Session;
 
@@ -38,16 +31,18 @@ public class HospedagemDao implements DaoGenerico<Hospedagem>{
     @Override
     public void salvar(Hospedagem h) throws SQLException{
         
+        ApartamentoDao apDao = new ApartamentoDao();
         String sql = "INSERT INTO hospedagem (idHospede, idUsuario, numeroAP, acompanhantes, dataEntrada,  status) VALUES (?, ?, ?, ?, ?, ?)";
         conn = ConnectionFactory.getConnection();
         ps = conn.prepareStatement(sql);
         ps.setInt(1, h.getHospede().getID());
         ps.setInt(2, usuarioLogado.getId());
-        ps.setString(3, h.getNumeroAP());
+        ps.setString(3, h.getNumeroAP().getNumero());
         ps.setString(4, h.getQtdAcompanhante());
         LocalDate now = LocalDate.now();
         ps.setDate(5, java.sql.Date.valueOf(now));
         ps.setBoolean(6, true);
+        ps.executeUpdate();
         
     }
                         
@@ -65,24 +60,52 @@ public class HospedagemDao implements DaoGenerico<Hospedagem>{
     @Override
     public List<Hospedagem> listar() throws SQLException {
         List<Hospedagem> lHospedagem = new ArrayList<>();
+         Hospedagem h = null;
+         HospedeDao hDao = null;
+         UsuarioDao userDao = null;
+         ApartamentoDao apDao = null;
         String sql = "SELECT * FROM hospedagem WHERE status = '1'";
         conn = ConnectionFactory.getConnection();
         ps = conn.prepareStatement(sql);
         ResultSet rs = ps.executeQuery();
         while(rs.next()){
-            Hospedagem h = new Hospedagem();
-            HospedeDao hDao = new HospedeDao();
+            h = new Hospedagem();
+            hDao = new HospedeDao();
             h.setHospede(hDao.buscar(rs.getInt("idHospede")));
-            h.setUsuario(usuarioLogado);
-            h.setNumeroAP(rs.getString("numeroAP"));
+            h.setUsuario(userDao.buscar(rs.getInt("idUsuario")));
+            h.setNumeroAP(apDao.buscar(rs.getInt("apartamento")));
             h.setQtdAcompanhante(rs.getString("acompanhantes"));
             h.setIdReserva(rs.getInt("idReserva"));
             h.setStatus(rs.getBoolean("status"));
         }
         return lHospedagem;
     }
-
-   
-   
     
+    
+    public Hospedagem buscar(Integer id)throws SQLException{
+        Hospedagem h = null;
+        HospedeDao hospede = null;
+        UsuarioDao user = null;
+        ApartamentoDao apDao = null;
+        String sql = "SELECT * FROM hospedagem WHERE id = ?";
+        conn = ConnectionFactory.getConnection();
+        ps = conn.prepareStatement(sql);
+        ps.setInt(1, id);
+        ResultSet rs = ps.executeQuery();
+        while(rs.next()){
+            h = new Hospedagem();
+            hospede = new HospedeDao();
+            apDao = new ApartamentoDao();
+            h.setId(rs.getInt("id"));
+            h.setHospede(hospede.buscar(rs.getInt("idHospede")));
+            h.setUsuario(user.buscar(rs.getInt("idUsuario")));
+            h.setNumeroAP(apDao.buscar(rs.getInt("numeroAP")));
+            h.setQtdAcompanhante(rs.getString("acompanhantes"));
+            h.setIdReserva(rs.getInt("idReserva"));
+            h.setStatus(rs.getBoolean("status"));
+            h.setDataEntrada(rs.getDate("dataEntrada"));
+        }
+        return h;
+    }
+  
 }
